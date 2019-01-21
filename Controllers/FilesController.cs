1cf407973d6e20df6a6b5fd04ac48eeecf9fd0d7
apollo78124 <98apollo@naver.com>
2019-01-21 14:07:07 -0800

    constructor

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -17,20 +17,20 @@ namespace lab1.Controllers
 
         public FilesController(IHostingEnvironment hostingEnvironment)
         {
-            path = hostingEnvironment.WebRootPath + @"/wwwroot/TextFiles";
+            path = hostingEnvironment.WebRootPath + @"/wwwroot/TextFiles/";
         }
         public IActionResult Index()
         {
             
 
             //Assuming Test is your Folder
-            string[] fileName = Directory.GetFiles("./wwwroot/TextFiles/"); //Getting Text files
+            string[] fileName = Directory.GetFiles(path); //Getting Text files
            // var fileList
            //     = from p in fileNames
             //      select p;
             string[] files = new string[fileName.Length];
            for(var i = 0 ; i < files.Length; i++){
-               files[i] = fileName[i].Split("./wwwroot/TextFiles/")[1].Split(".txt")[0];
+               files[i] = fileName[i].Split(path)[1].Split(".txt")[0];
            }
             ViewData.Model = files;
 
@@ -39,8 +39,8 @@ namespace lab1.Controllers
 
         public IActionResult Contents(string id)
         {   
-            string path = "./wwwroot/TextFiles/" + id + ".txt";
-            var lines = System.IO.File.ReadAllLines(path);
+            string path1 = path + id + ".txt";
+            var lines = System.IO.File.ReadAllLines(path1);
             Console.Write(lines);
             return View(lines);
         }
